f0ec700ecd6f9000bd8250eb22a405d4adc6038d
Ron van Wier <ron@van-wier.org>
2020-05-01 17:25:06 +0200

    string emcoding specified

--- http_get_time.py
+++ http_get_time.py
@@ -2,7 +2,7 @@ import json
 import urllib.request
 
 f = urllib.request.urlopen('http://worldtimeapi.org/api/ip')
-result = f.readline()
+result = str(f.readline(),encoding='utf-8')
 print(result)
 print("===========\n")
 fields = json.loads(result)
