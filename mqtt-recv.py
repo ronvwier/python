6b7d185d55d92c5b16209511cbb2e1f1d8adde24
Ron van Wier <ron@van-wier.org>
2020-05-09 18:44:07 +0200

    server name for mqtt = server

--- mqtt-recv.py
+++ mqtt-recv.py
@@ -9,10 +9,11 @@ def on_message(client, userdata, msg):
 	#The callback for when a PUBLISH message is received from the server.
     print("Message received-> " + msg.topic + " " + str(msg.payload))
 
+SERVER = 'server'
+#SERVER = 'mqtt.eclipse.org'
+
 client = mqtt.Client("digi_mqtt_test")  # Create instance of client with ID
 client.on_connect = on_connect
 client.on_message = on_message
-# client.connect("m2m.eclipse.org", 1883, 60)  
-# Connect to (broker, port, keepalive-time)
-client.connect('192.168.0.129', 1883,60)
+client.connect(SERVER)
 client.loop_forever()  # Start networking daemon
--- mqtt-send.py
+++ mqtt-send.py
@@ -1,9 +1,11 @@
 import paho.mqtt.client as mqtt
 
-# This is the Publisher
+SERVER = 'server'
+#SERVER = 'mqtt.eclipse.org'
 
+# This is the Publisher
 client = mqtt.Client()
-client.connect("192.168.0.129",1883,60)
-client.publish("test/message", "Hello windows world! #1",retain=True)
-client.publish("test/message", "Hello windows world! #2",retain=True)
+client.connect(SERVER)
+client.publish("test/message", "Hello windows world! #1 on " + SERVER,retain=True)
+client.publish("test/message", "Hello windows world! #2 on " + SERVER,retain=True)
 client.disconnect()
