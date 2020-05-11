200b0ab728cb066bb119ed48b131bc4c80c9aad0
Ron van Wier <ron@van-wier.org>
2020-05-11 15:55:24 +0200

    python exec as script at raspberry pi

--- mqtt_time.py
+++ mqtt_time.py
@@ -1,13 +1,10 @@
+#!/usr/bin/python3
 import datetime
 import paho.mqtt.client as mqtt
 
-# Get date and time
-x = datetime.datetime.now()
-print(x)
-
 # Format message as Mon 11 May 2020,14:56
+x = datetime.datetime.now()
 message = x.strftime('%a %d %b %Y,%H:%M')
-print(message)
 
 # Publish to MQTT server
 SERVER = '192.168.0.129'
