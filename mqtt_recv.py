166fee404dde10dbf7699e699c495c45caf099c1
Ron van Wier <ron@van-wier.org>
2020-05-19 15:14:59 +0200

    omnik change ad mqtt recv updated

--- mqtt_recv.py
+++ mqtt_recv.py
@@ -3,17 +3,22 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
 	#The callback for when the client connects to the broker
     print("Connected with result code {0}".format(str(rc)))
-    client.subscribe("test/message")
+    client.subscribe([('time/display',0),('env/out',0),('inverter/omnik',0)])
 
 def on_message(client, userdata, msg):
 	#The callback for when a PUBLISH message is received from the server.
-    print("Message received-> " + msg.topic + " " + str(msg.payload))
+	print("Message received-> " + msg.topic + " " + str(msg.payload))
+   
+def on_log(client, userdata, level, buf):
+    print("log: ",buf) 
 
-SERVER = 'server'
-#SERVER = 'mqtt.eclipse.org'
-
-client = mqtt.Client("digi_mqtt_test")  # Create instance of client with ID
+client = mqtt.Client("",clean_session=True)  # Create instance of client with ID
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(SERVER)
-client.loop_forever()  # Start networking daemon
+client.on_log=on_log
+
+client.connect('192.168.0.129')
+
+while True:
+	print('loop')
+	client.loop()  # Start networking daemon
--- test_dict.py
+++ test_dict.py
@@ -1,5 +1,5 @@
 import json
-x = { "a":"alfa","b":"beta", "c":'gamma' }
+x = { "a":"alfa","b":"beta", "c":'gamma', "d": "destroy"  }
 print(x)
 s = json.dumps(x)
 print(s)
