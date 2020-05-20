f3ae8cd03c7c2f55ebaa0b2702b659aa9d00e857
Ron van Wier <ron@van-wier.org>
2020-05-20 14:20:10 +0200

    done

--- mqtt_recv.py
+++ mqtt_recv.py
@@ -2,8 +2,7 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
 	#The callback for when the client connects to the broker
-    print("Connected with result code {0}".format(str(rc)))
-    client.subscribe([('time/display',0),('env/out',0),('inverter/omnik',0)])
+    client.subscribe([('time/#',0),('env/#',0),('inverter/#',0)])
 
 def on_message(client, userdata, msg):
 	#The callback for when a PUBLISH message is received from the server.
@@ -15,7 +14,7 @@ def on_log(client, userdata, level, buf):
 client = mqtt.Client("",clean_session=True)  # Create instance of client with ID
 client.on_connect = on_connect
 client.on_message = on_message
-client.on_log=on_log
+#client.on_log=on_log
 
 client.connect('192.168.0.129')
 
