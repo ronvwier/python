f13c9eb84ffd32dc16f3af4baf4592f04a883e7a
Ron van Wier <ron@van-wier.org>
2020-05-23 16:26:53 +0200

    basic test

--- pandas_basic.py
+++ pandas_basic.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
-seen = pd.read_excel('pandas_in.xlsx', sheet_name='seen', index_col='license')
-owners = pd.read_excel('pandas_in.xlsx', sheet_name='owners', index_col='license')
-
-all = seen.join(owners)
-print(all)
-
 df = pd.DataFrame(
 {"a" : [4 ,5, 6],
 "b" : [7, 8, 9],
@@ -13,9 +7,4 @@ df = pd.DataFrame(
 index = [1, 2, 3])
 print(df)
 
-with pd.ExcelWriter('pandas_out.xlsx') as writer:  
-    all.to_excel(writer, sheet_name='All')
-    seen.to_excel(writer, sheet_name='Seen')
-    owners.to_excel(writer, sheet_name='Owners')
-
-#print(df.describe())
+print(df.describe())
