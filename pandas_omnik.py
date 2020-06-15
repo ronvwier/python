090992500d4d2c6cb2d5d2375f832624c4bc7a8a
Ron van Wier <ron@van-wier.org>
2020-06-15 17:02:31 +0200

    omnik pandas minimised

--- pandas_omnik.py
+++ pandas_omnik.py
@@ -7,13 +7,10 @@ Read the OMNIK values and calculate max per hour
 import pandas as pd
 
 # Read the input excel files skip 8 header lines
-df = pd.read_excel('pandas_omnik.xlsx', skiprows=8 , skipfooter=3)
-
-df2 = df.rename(columns={"Time(GMT +1)": "Time"})
+df = pd.read_excel('pandas_omnik.xlsx', skiprows=8 , skipfooter=3).rename(columns={"Time(GMT +1)": "Time"})
 
 # Calculate the hour and correct for summertime (GMT+2)
-df2['Hour'] = pd.to_numeric(df2['Time'].str[7:9]) +1
+df['Hour'] = pd.to_numeric(df['Time'].str[7:9]) +1
 
 # Obtain the max values (Pac1 = power)
-df4 = df2.set_index(['Hour','Time'])
-print(df4.max(level='Hour'))
+print(df.set_index('Hour').filter(items=['Hour', 'Pac1']).max(level='Hour'))
