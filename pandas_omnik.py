1a7f4caa2214f69ffef3167fbd005ffff2238d79
Ron van Wier <ron@van-wier.org>
2020-06-15 14:50:18 +0200

    omnik excel finished

--- pandas_omnik.py
+++ pandas_omnik.py
@@ -1,24 +1,19 @@
 '''
 Read OMNIK inverter excel sheets with pandas
 
-Just a test script
+Read the OMNIK values and calculate max per hour
 '''
 
 import pandas as pd
-
-def fill_hour(df):
-    """
-    Chicago, IL -> Chicago for city_name column
-    """
-    df['Hour'] = df['Time'].str[7:9]
-    #df['Hour'] = df['Hour'] +1
-    return df
-
 
 # Read the input excel files skip 8 header lines
 df = pd.read_excel('pandas_omnik.xlsx', skiprows=8 , skipfooter=3)
 
 df2 = df.rename(columns={"Time(GMT +1)": "Time"})
-df3 = fill_hour(df2)
-df4 = df3.set_index(['Hour','Time'])
+
+# Calculate the hour and correct for summertime (GMT+2)
+df2['Hour'] = df2['Time'].str[7:9].astype(int) +1
+
+# Obtain the max values (Pac1 = power)
+df4 = df2.set_index(['Hour','Time'])
 print(df4.max(level='Hour'))
