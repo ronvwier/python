3d6d6ca02635ac32bb060371928f31359f34e5e8
Ron van Wier <ron@van-wier.org>
2020-07-09 17:13:25 +0200

    sysinfo

--- sysinfo.py
+++ sysinfo.py
@@ -1,10 +1,14 @@
 import sys
 
-print(sys.platform)        # win32 / esp32 / linux
-print(sys.implementation)  # name=micropython / cpython 
-print(sys.path)
-print(sys.version)
+print('sys.platform:',sys.platform)        # win32 / esp32 / linux
+print('sys.implementation:',sys.implementation)  # name=micropython / cpython 
+print('sys.path:',sys.path)
+print('sys.version:',sys.version)
 
 if sys.platform == "esp32":
     import machine
-    print(machine.unique_id())
+    print('machine.unique_id:',machine.unique_id())
+else:
+    import platform
+    print('platform.node:',platform.node())     # toshiba-ron
+    print('platform.system:',platform.system())   # Windows / Linux 
